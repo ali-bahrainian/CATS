47e257d48c26d3fad72adac5661933ed2d4abb16
Martijn Schouten <martijn@Martijns-MacBook-Pro.local>
2021-10-11 14:15:20 +0200

    fix: typeerror due to migration

--- run_summarization.py
+++ run_summarization.py
@@ -294,7 +294,7 @@ def main(unused_argv):
   # Make a namedtuple hps, containing the values of the hyperparameters that the model needs
   hparam_list = ['mode', 'lr', 'adagrad_init_acc', 'rand_unif_init_mag', 'trunc_norm_init_std', 'max_grad_norm', 'hidden_dim', 'emb_dim', 'batch_size', 'max_dec_steps', 'max_enc_steps', 'coverage', 'cov_loss_wt', 'pointer_gen']
   hps_dict = {}
-  for key,val in FLAGS.__flags.items(): # for each flag
+  for key,val in FLAGS.flag_values_dict().items(): # for each flag
     if key in hparam_list: # if it's in the list
       hps_dict[key] = val # add it to the dict
   hps = namedtuple("HParams", list(hps_dict.keys()))(**hps_dict)
