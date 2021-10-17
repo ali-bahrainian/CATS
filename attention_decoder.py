deaf0010c4625d43490344c89b3e9d61423cd851
Martijn Schouten <martijn.schouten@icloud.com>
2021-10-17 13:52:51 +0200

    fix: attention size definition

--- attention_decoder.py
+++ attention_decoder.py
@@ -50,7 +50,7 @@ def attention_decoder(decoder_inputs, initial_state, encoder_states, enc_topicwo
   """
   with variable_scope.variable_scope("attention_decoder") as scope:
     batch_size = encoder_states.get_shape()[0] # if this line fails, it's because the batch size isn't defined
-    attn_size = encoder_states.get_shape()[2].value # if this line fails, it's because the attention length isn't defined
+    attn_size = encoder_states.get_shape()[2] # if this line fails, it's because the attention length isn't defined
     
     #tf.Print(attn_dist, [attn_dist])
     print("-------------------------")
